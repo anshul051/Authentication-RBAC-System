2b1bd941c55a27c4d719ba758e9d483ab5926a0a
anshul051 <anshulkumararya51@gmail.com>
2026-01-29 18:23:58 +0530

    u

--- server/src/utils/password.util.js
+++ server/src/utils/password.util.js
@@ -12,12 +12,6 @@ export const hashPassword = async (password) => {
     return hashedPassword;
 };
 
-/**
- * Compare plain text password with hashed password
- * @param {string} password - Plain text password from login
- * @param {string} hashedPassword - Hashed password from database
- * @returns {Promise<boolean>} - True if passwords match
- */
 export const comparePassword = async (password, hashedPassword) => {
     const isMatch = await bcrypt.compare(password, hashedPassword);
     return isMatch;
